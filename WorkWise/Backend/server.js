d5ec3710fe232c77f62ff6fd9840898c4853a200
Shiv Sahni <shivsahni2240@gmail.com>
2025-04-30 18:16:37 +0530

    CORS fixed

--- WorkWise/Backend/server.js
+++ WorkWise/Backend/server.js
@@ -8,10 +8,7 @@ const cors = require("cors")
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-    origin: 'https://int-pre.onrender.com',
-    credentials: true
-  }));
+app.use(cors());
 app.use("/api/auth/", router)
 app.use("/api/auth/seat", router2)
 
