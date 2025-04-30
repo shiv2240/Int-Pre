1c92be4fc409569388935aa8d03a2e2897295c37
Shiv Sahni <shivsahni2240@gmail.com>
2025-04-30 09:51:40 +0530

    Cors

--- WorkWise/Backend/server.js
+++ WorkWise/Backend/server.js
@@ -8,7 +8,10 @@ const cors = require("cors")
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://workwise-intpre.netlify.app',
+    credentials: true
+  }));
 app.use("/api/auth/", router)
 app.use("/api/auth/seat", router2)
 
